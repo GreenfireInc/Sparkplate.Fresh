923f58e829cfba4b0f86e6a5320cfdc2cd6b7018
Corey Stedman <cordesir@gmail.com>
2025-05-23 19:45:00 -0400

    Added the NavBar from Sparkplate v1, but its not enabled. Will do in the next branch

--- src/demos/ipc.ts
+++ src/demos/ipc.ts
@@ -1,4 +1,6 @@
-
-window.ipcRenderer.on('main-process-message', (_event, ...args) => {
-  console.log('[Receive Main-process message]:', ...args)
-})
+// Make sure ipcRenderer is available before accessing it
+if (window.ipcRenderer) {
+  window.ipcRenderer.on('main-process-message', (_event, ...args) => {
+    console.log('[Receive Main-process message]:', ...args)
+  })
+}
