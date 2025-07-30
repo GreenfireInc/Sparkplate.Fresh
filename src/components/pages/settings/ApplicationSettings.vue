4a7564949f6fd9a6c43cbaac5bf9d0cc32540b78
Corey Stedman <cordesir@gmail.com>
2025-07-29 21:24:55 -0400

    Added a close to tray option, but not enabled

--- src/components/pages/settings/ApplicationSettings.vue
+++ src/components/pages/settings/ApplicationSettings.vue
@@ -47,6 +47,17 @@
             Express (Access via {{ localIp }}:3000)
           </label>
         </div>
+
+        <div class="flex items-center justify-between p-4 bg-gray-50 rounded-lg">
+          <div>
+            <p class="font-medium">Close to tray</p>
+            <p class="text-sm text-gray-600">Minimize to system tray when closed</p>
+          </div>
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" v-model="closeToTray" class="sr-only peer">
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+          </label>
+        </div>
       </div>
     </div>
   </div>
@@ -57,7 +68,8 @@ export default {
   name: 'ApplicationSettings',
   data() {
     return {
-      localIp: ''
+      localIp: '',
+      closeToTray: false
     }
   },
   mounted() {
